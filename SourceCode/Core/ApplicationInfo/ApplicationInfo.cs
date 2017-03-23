4185e59be15288326394501f7cf33c424d4963cb
Ehryk <rhaistlin@gmail.com>
2017-03-23 17:23:39 -0500

    Core: Added BuildDate property to ApplicationInfo

--- SourceCode/Core/ApplicationInfo/ApplicationInfo.cs
+++ SourceCode/Core/ApplicationInfo/ApplicationInfo.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Reflection;
 
 namespace Core.ApplicationInfo
@@ -6,6 +7,14 @@ namespace Core.ApplicationInfo
     public static class ApplicationInfo
     {
         public static Version Version { get { return Assembly.GetCallingAssembly().GetName().Version; } }
+
+        public static int Major { get { return Assembly.GetCallingAssembly().GetName().Version.Major; } }
+
+        public static int Minor { get { return Assembly.GetCallingAssembly().GetName().Version.Minor; } }
+
+        public static int Build { get { return Assembly.GetCallingAssembly().GetName().Version.Build; } }
+
+        public static int Revision { get { return Assembly.GetCallingAssembly().GetName().Version.Revision; } }
 
         public static string Title
         {
@@ -17,7 +26,7 @@ namespace Core.ApplicationInfo
                     AssemblyTitleAttribute titleAttribute = (AssemblyTitleAttribute)attributes[0];
                     if (titleAttribute.Title.Length > 0) return titleAttribute.Title;
                 }
-                return System.IO.Path.GetFileNameWithoutExtension(Assembly.GetExecutingAssembly().CodeBase);
+                return Path.GetFileNameWithoutExtension(Assembly.GetExecutingAssembly().CodeBase);
             }
         }
 
@@ -26,7 +35,7 @@ namespace Core.ApplicationInfo
             get
             {
                 object[] attributes = Assembly.GetCallingAssembly().GetCustomAttributes(typeof(AssemblyProductAttribute), false);
-                return attributes.Length == 0 ? "" : ((AssemblyProductAttribute)attributes[0]).Product;
+                return attributes.Length == 0 ? null : ((AssemblyProductAttribute)attributes[0]).Product;
             }
         }
 
@@ -35,7 +44,7 @@ namespace Core.ApplicationInfo
             get
             {
                 object[] attributes = Assembly.GetCallingAssembly().GetCustomAttributes(typeof(AssemblyDescriptionAttribute), false);
-                return attributes.Length == 0 ? "" : ((AssemblyDescriptionAttribute)attributes[0]).Description;
+                return attributes.Length == 0 ? null : ((AssemblyDescriptionAttribute)attributes[0]).Description;
             }
         }
 
@@ -44,7 +53,7 @@ namespace Core.ApplicationInfo
             get
             {
                 object[] attributes = Assembly.GetCallingAssembly().GetCustomAttributes(typeof(AssemblyCopyrightAttribute), false);
-                return attributes.Length == 0 ? "" : ((AssemblyCopyrightAttribute)attributes[0]).Copyright;
+                return attributes.Length == 0 ? null : ((AssemblyCopyrightAttribute)attributes[0]).Copyright;
             }
         }
 
@@ -53,8 +62,33 @@ namespace Core.ApplicationInfo
             get
             {
                 object[] attributes = Assembly.GetCallingAssembly().GetCustomAttributes(typeof(AssemblyCompanyAttribute), false);
-                return attributes.Length == 0 ? "" : ((AssemblyCompanyAttribute)attributes[0]).Company;
+                return attributes.Length == 0 ? null : ((AssemblyCompanyAttribute)attributes[0]).Company;
             }
+        }
+
+        public static DateTime BuildDate { get { return Assembly.GetCallingAssembly().GetLinkerTime(); } }
+
+        public static DateTime GetLinkerTime(this Assembly assembly, TimeZoneInfo target = null)
+        {
+            var filePath = assembly.Location;
+            const int c_PeHeaderOffset = 60;
+            const int c_LinkerTimestampOffset = 8;
+
+            var buffer = new byte[2048];
+
+            using (var stream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+                stream.Read(buffer, 0, 2048);
+
+            var offset = BitConverter.ToInt32(buffer, c_PeHeaderOffset);
+            var secondsSince1970 = BitConverter.ToInt32(buffer, offset + c_LinkerTimestampOffset);
+            var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+            var linkTimeUtc = epoch.AddSeconds(secondsSince1970);
+
+            var tz = target ?? TimeZoneInfo.Local;
+            var localTime = TimeZoneInfo.ConvertTimeFromUtc(linkTimeUtc, tz);
+
+            return localTime;
         }
     }
 }
